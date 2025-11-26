13536af05a345cfaf1d22d61c55931f40d132a23
Gleb Ermukov <yermukov@list.ru>
2025-11-26 12:30:41 +0200

    Исправление route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import Taimer from "./components/Page main/Taimer/Taimer";
 import About from "./components/Page About/About/About";
 import Header from "./Header/Header";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
+
 const App = () => {
   return (
     <>
-      <Header />
-
-      {/* <BrowserRouter basename="/">
+      <HashRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Taimer />} />
           <Route path="/aboutProject" element={<About />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter> */}
+      </HashRouter>
     </>
   );
 };
+
 export default App;
